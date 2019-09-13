638453afbd5453c95bf8acfde00066a35e3f4c0c
Your name <a.m.verdine@gmail.com>
2019-09-13 15:57:38 -0400

    Setup state array, form, and form fields

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
-import UserForm from './components/Form';
+import OnboardForm from './components/Form';
 
 function App() {
+
+  const [userForm, setUserForm] = useState([
+    {
+      name: "Aaron Verdine",
+      email: "a_verdine118@yahoo.com",
+      password: "",
+      TOS: false
+    }
+  ]);
+
+
   return (
     <div className="App">
-      <UserForm />
+    <h1> Team Onboarding Form</h1>
+      <OnboardForm />
+
+      {userForm.map((userForm, index) => (
+        <div className="user-form" key={index}>
+        <h2>{userForm.name}</h2>
+        <h3>{userForm.email}</h3>
+        <h4>{userForm.password}</h4>
+        <h4>{userForm.TOS}</h4>
+
+        </div>
+      ))}
     </div>
   );
 }
--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -1,13 +1,58 @@
-import React from 'react';
+import React, { useState } from "react";
 
-const UserForm = () => {
+const OnboardForm = props => {
+  console.log(props);
 
+  const formFields = {
+    name: "",
+    email: "",
+    password: "",
+    TOS: false
+  };
 
-    return (
-        <div>
-            <h1>Hello Aaron!</h1>
-        </div>
-    )
-}
+  const [newUser, setNewUser] = { formFields };
 
-export default UserForm;
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="name"
+          placeholder="Name"
+          onChange={handleChange}
+          value={newUser.name}
+        />
+        <br></br>
+        <input
+          type="text"
+          name="email"
+          placeholder="Email"
+          onChange={handleChange}
+          value={newUser.email}
+        />
+        <br></br>
+        <input
+          type="password"
+          name="password"
+          placeholder="Enter Passwrod"
+          onChange={handleChange}
+          value={newUser.password}
+        />
+        <br></br>
+        <input
+          type="radio"
+          name="Terms of Service"
+          onChange={handleChange}
+          value={newUser.TOS}
+        />
+        <br></br>
+
+        <button type="submit" onClick="alert('Welcome Aboard!')">
+          Submit Here!
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default OnboardForm;
