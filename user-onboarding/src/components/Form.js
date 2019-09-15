83c05a8a252fbdf46c30a34b51c316796591175e
Your name <a.m.verdine@gmail.com>
2019-09-14 23:29:13 -0400

    Initiate axios POST request to reqres API

--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -1,26 +1,31 @@
 import React from "react";
 import { withFormik, Form, Field } from "formik";
 import * as yup from "yup";
+import axios from "axios";
 
-const OnboardForm = ({ errors }) => {
+const OnboardForm = ({ errors, touched }) => {
   // console.log(props);
   return (
     <div>
       <Form>
-        {errors.name && <p className="error">{errors.name}</p>}
+        {touched.name && errors.name && <p className="error">{errors.name}</p>}
         <Field type="text" name="name" placeholder="Name" />
         <br></br>
 
-        {errors.email && <p className="error">{errors.email}</p>}
+        {touched.email && errors.email && (
+          <p className="error">{errors.email}</p>
+        )}
         <Field type="text" name="email" placeholder="Email" />
         <br></br>
 
-        {errors.password && <p className="error">{errors.password}</p>}
-        
+        {touched.password && errors.password && (
+          <p className="error">{errors.password}</p>
+        )}
+
         <Field type="password" name="password" placeholder="Enter Password" />
         <br></br>
 
-        {errors.TOS && <p className="error">{errors.TOS}</p>}
+        {touched.TOS && errors.TOS && <p className="error">{errors.TOS}</p>}
         <label>
           <Field type="checkbox" name="TOS" />
           <span>Terms of Service</span>
@@ -36,21 +41,30 @@ export default withFormik({
   mapPropsToValues: values => {
     return {
       name: values.name || "",
-      email: values.email || "", 
+      email: values.email || "",
       password: values.password || "",
       TOS: values.TOS || false
     };
   },
   validationSchema: yup.object().shape({
-    name: yup.string().required(),
+    name: yup.string().required("Please tell us your name"),
     email: yup
       .string()
-      .required()
+      .required("Please provide a valid email address")
       .email(),
-    password: yup.string().required(),
-    TOS: yup.boolean().oneOf([true])
+    password: yup.string().required("Please enter the correct password"),
+    TOS: yup
+      .boolean()
+      .oneOf([true], "Please accept the Terms of Service to continue   ")
   }),
   handleSubmit: values => {
-    console.log(values);
+    axios
+      .post("https://reqres.in/api/users", values)
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
 })(OnboardForm);
